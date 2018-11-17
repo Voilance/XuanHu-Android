1c527d18843a6ab64f48a5c3f33197792d8ddae4
Yilin Chen <sticnarf@gmail.com>
2018-11-17 16:01:37 +0800

    Add cookie support

--- app/src/test/java/cn/biketomotor/xh/xuanhu/Api/SessionApiUnitTest.java
+++ app/src/test/java/cn/biketomotor/xh/xuanhu/Api/SessionApiUnitTest.java
@@ -3,8 +3,6 @@ package cn.biketomotor.xh.xuanhu.Api;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 public class SessionApiUnitTest {
@@ -26,5 +24,22 @@ public class SessionApiUnitTest {
         String password = "testtest2";
         Result<SessionApi.LoginResult> result = SessionApi.INSTANCE.login(email, password);
         assertTrue(result.isErr() || (!result.get().success));
+    }
+
+    @Test
+    public void logoutBeforeLoginShouldFail() {
+        SessionApi.INSTANCE.logout();
+        Result<SessionApi.LogoutResult> result = SessionApi.INSTANCE.logout();
+        assertTrue(result.isErr() || (!result.get().success));
+    }
+
+    @Test
+    public void shouldLogoutAfterLogin() {
+        String email = "test@test.com";
+        String password = "testtest";
+        Result<SessionApi.LoginResult> result1 = SessionApi.INSTANCE.login(email, password);
+        assertTrue(result1.isOk());
+        Result<SessionApi.LogoutResult> result2 = SessionApi.INSTANCE.logout();
+        assertTrue(result2.isOk());
     }
 }
